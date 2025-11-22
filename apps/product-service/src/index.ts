77bbd3d31ead7e90b4f8e860a76c91dd645d23ff
Oliver Metz <softwaredevdad@gmail.com>
2025-11-22 19:31:07 +0100

    add some commands to our package.json for the product-service, add simple backend server and cors origins.

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.listen(3000, () => {
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  }),
+);
+
+app.listen(8000, () => {
   console.log("Server is running on port 3000");
-  __dirname;
 });
