1e6d2786b96fc1122d43c9dae4231af75e1e3b27
Oliver Metz <softwaredevdad@gmail.com>
2025-11-24 10:32:20 +0100

    Implement update category, delete category and get All categories controllers. - API's tested via Requestly

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -23,7 +23,73 @@ export const createCategory = catchAsync(
 );
 //#endregion
 
-export const updateCategory = async (req: Request, res: Response) => {};
-export const deleteCategory = async (req: Request, res: Response) => {};
-export const getCategories = async (req: Request, res: Response) => {};
-export const getCategory = async (req: Request, res: Response) => {};
+//#region PUT: Update Category
+export const updateCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const data: Prisma.CategoryUpdateInput = req.body;
+
+    if (!id) {
+      return next(new AppError("Category ID Not Found", 400));
+    }
+
+    const category = await prisma.category.update({
+      where: { id: Number(id) },
+      data,
+    });
+
+    if (!category) {
+      return next(new AppError("Category Failed To Update", 400));
+    }
+
+    return res.status(200).json({
+      success: true,
+      category,
+    });
+  },
+);
+//#endregion
+
+//#region DELETE: Delete Category
+export const deleteCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    if (!id) {
+      return next(new AppError("Category ID Not Found", 400));
+    }
+
+    const categoryToDelete = await prisma.category.delete({
+      where: { id: Number(id) },
+    });
+
+    if (!categoryToDelete) {
+      return next(new AppError("Category Failed To Delete", 400));
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Category Deleted",
+    });
+  },
+);
+//#endregion
+
+//#region GET: Get All Categories
+export const getCategories = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const categories = await prisma.category.findMany();
+
+    if (!categories || categories.length === 0) {
+      return next(
+        new AppError(
+          "Categories Not Found - Categories Must contain at least one category",
+          400,
+        ),
+      );
+    }
+
+    return res.status(200).json({ success: true, categories });
+  },
+);
+//#endregion
