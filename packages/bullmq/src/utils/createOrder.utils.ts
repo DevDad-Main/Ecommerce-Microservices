f9a1e9e4e98a567828233d967d433aa09789fb37
Oliver Metz <softwaredevdad@gmail.com>
2025-11-27 21:57:12 +0100

    add our mongoose connection check in our createOrder function.
    This is the wrong place to put it, need to refactor it tomorrow and put
    it inside our worker as we get errors due to the string being undefined
    momentarily because our worker tries to process everyhthing before our
    MONGO_URL is defined and tehn finally processes, so we need to add those
    checks into our worker so it dosen't process anything until connected.
    Check BullMq docs.

--- packages/bullmq/src/utils/createOrder.utils.ts
+++ packages/bullmq/src/utils/createOrder.utils.ts
@@ -1,7 +1,14 @@
+import "dotenv/config";
 import { OrderType } from "@repo/types";
 import { Order } from "@repo/order-db";
+import mongoose from "mongoose";
 
 export const createOrder = async (order: OrderType) => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.MONGO_URL!);
+    console.log("Worker connected to MongoDB");
+  }
+
   const newOrder = new Order(order);
 
   try {
