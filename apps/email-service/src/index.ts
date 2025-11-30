7baf222fbba4646d4fb05833662900a388abf9e1
Oliver Metz <softwaredevdad@gmail.com>
2025-11-30 14:47:08 +0100

    simple test IIFE to debug our emailing service, successfully works. Now to implement BullMQ for sending emails.

---
+++ apps/email-service/src/index.ts
@@ -0,0 +1,13 @@
+import sendEmail from "./utils/nodemailer.utils";
+//
+// (async () => {
+//   try {
+//     await sendEmail({
+//       toEmail: "softwaredevdad@gmail.com",
+//       subject: "Hello",
+//       text: "Hello World",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
