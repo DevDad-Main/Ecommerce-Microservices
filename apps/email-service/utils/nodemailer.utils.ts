53df9f449447f5beeac5d53327d5f0b718470578
Oliver Metz <softwaredevdad@gmail.com>
2025-11-30 14:38:39 +0100

    add nodemailer config setup with .env vars for google OAuth

--- apps/email-service/utils/nodemailer.utils.ts
+++ apps/email-service/utils/nodemailer.utils.ts
@@ -4,9 +4,30 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: "me@gmail.com",
+    user: "softwaredevdad@gmail.com",
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
 });
+
+const sendEmail = async ({
+  toEmail,
+  subject,
+  text,
+}: {
+  toEmail: string;
+  subject: string;
+  text: string;
+}) => {
+  const res = await transporter.sendMail({
+    from: '"Oliver Metz" <softwaredevdad@gmail.com>',
+    to: toEmail,
+    subject,
+    text,
+  });
+
+  console.log("MESSAGE SENT: ", res);
+};
+
+export default sendEmail;
